c45dd2a82634d6fcf3b859726a9a966bf99d050d
unknown <lblondeau.megasoft@gmail.com>
2020-02-29 13:58:18 +0100

    creation des normes

--- src/auto_tester/norms/Absolute_error.java
+++ src/auto_tester/norms/Absolute_error.java
@@ -4,25 +4,22 @@ import auto_tester.interfaces.INorm;
 
 import java.util.List;
 
-public class Relative_error implements INorm {
+public class Absolute_error implements INorm {
     @Override
     /*Number_1 est la valeur obtenue après par la fonction de test et number_2 le RA*/
     public Number run(List<Number> number_1, List<Number> number_2) {
         double result=0 ;
-        double divisor=0 ;
         for (int i=0; i<number_1.size();i++) {
 
             if(number_2.get(i).doubleValue()!= 0) {
 
                 result =  result +Math.pow((number_1.get(i).doubleValue()-number_2.get(i).doubleValue()),2);
-                divisor  += Math.pow(number_2.get(i).doubleValue(),2);
 
             }
 
         }
+        return Math.sqrt(result);
 
-
-
-        return Math.sqrt(result/divisor);
     }
+
 }
